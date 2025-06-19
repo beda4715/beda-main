519e4983d84875daa55d6c36af838ea86a5fe6fc
방승규 <beda4150@gmail.com>
2025-06-19 14:03:18 +0900

    feat: fe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,9 @@ export default async function Home() {
     <div className='w-full flex flex-col items-center px-4 py-8'>
       <div className='max-w-6xl w-full grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-4 grid'>
         {posts.map((post) => (
-          <div key={post.slug} className='border rounded-lg shadow-md border-black/10'>
+          <div key={post.slug} className='border2 rounded-lg shadow-md border-black/10'>
             <div className='p-4'>
-              <h2 className='text-2xl font-bold mb-2'>
+              <h2 className='text-2xl font-bold hd'>
                 <Link href={`/posts/${post.slug}`}>{post.metadata.title}</Link>
               </h2>
               <div className='text-gray-600 mb-4'>{post.metadata.description}</div>
